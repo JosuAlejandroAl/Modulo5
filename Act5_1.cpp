d7ebe50f7feb5fc635b72e4a4dbd56eb34a95459
JosuAlejandroAl <92008810+JosuAlejandroAl@users.noreply.github.com>
2021-11-26 22:34:17 -0600

    Create Act5_1.cpp

---
+++ Act5_1.cpp
@@ -0,0 +1,108 @@
+#define HashMod 97
+#include<iostream>
+using namespace std;
+
+unsigned long long CharToInt(char entrada)
+{
+	unsigned long long Salida;
+	Salida = entrada;
+	return Salida;
+}
+unsigned long long ObtenerNumero(string entrada, string entradaM, string entradaMo, int entradaYear)
+{
+	unsigned long long Salida=0;
+	for(int i=0;entrada[i] != '\0';i++)
+	{
+		if(entrada[1+i] == '\0' && i==0)
+			Salida+=CharToInt(entrada[i]);
+		else
+		{
+			if(i==0)
+			{
+				Salida += CharToInt(entrada[i]);
+				continue;
+			}
+			else
+			{
+				
+				int miChar = CharToInt(entrada[i]);
+				if(miChar>99)
+				{
+					Salida*=1000;
+				}
+				else if(miChar>9)
+				{
+					Salida*=100;
+				}
+				
+				Salida += miChar;
+			}
+				
+			
+		}
+	}
+	return Salida;
+}
+
+int  FuncHash(string entrada, string entradaM,string entradaMo,int entradaYear)
+{
+	unsigned long long Amodular = ObtenerNumero(entrada, entradaM, entradaMo, entradaYear);
+	int Salida = Amodular%HashMod;
+	return Salida;
+}
+
+
+main()
+{
+	string Tabla[97];
+	for(int i = 0; i<97; i++)
+	{
+		Tabla[i]="";
+	}
+			
+	int OpcMen=0;
+	do
+	{
+		cout<<"---Menu hash---\n";
+		cout<<"1. insertar Placa\n";
+		cout<<"2. ver elementos Placa\n";
+		cout<<"Ingrese su opcion:";
+		
+		cin>>OpcMen;
+		
+		if(OpcMen == 1)
+		{
+			string Placa, Marca, Modelo; 
+			int year;
+
+			cout<<"ingrese su Placa: ";
+			cin>>Placa>>Marca>>Modelo>>year;
+			int Pos = FuncHash(Placa, Marca, Modelo, year);
+			cout<<"\nValor hash: "<<Pos<<" "<<Placa<<" "<<Marca<<" "<<Modelo<<" "<<year;
+			if(Tabla[Pos]=="null" || Tabla[Pos]==Placa, Marca, Modelo, year)
+				Tabla[Pos]=Placa, Marca, Modelo, year;
+			else
+				for(int i=Pos;i<HashMod;i++)
+					if(Tabla[i]=="NULL")
+					{
+						Tabla[i]=Placa, Marca, Modelo, year;
+						break;
+					}
+						
+			
+		}
+		
+		if(OpcMen == 2)
+		{
+			for(int i = 0; i<97; i++)
+			{
+				cout<<i<<" "<<Tabla[i]<<"\n";
+			}
+		}
+
+	cout<<"\n\n";
+		
+	}while(OpcMen!=0);
+	
+	
+}
